5158eaca8069127f44e3b86b34ced9fecd851a76
Bhavesh Patil <dadu.patil@gmail.com>
2022-10-17 03:09:27 +0530

    clear

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-import requests
 
-url = "https://rest.fnar.net/auth/login"
-data = {"UserName": "GladeOwl", "Password": "v*DY8k8W6zVEd22"}
-
-response = requests.post(url=url, json=data)
-print(response.text)
